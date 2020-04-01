67468bae0331ef77bf4a8d7c7e78b41361114573
Rafael Benevides <rafabene@gmail.com>
2020-04-01 14:38:21 -0300

    Added a Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM openjdk:11-oraclelinux7 as builder
+
+WORKDIR /app
+
+COPY first/target/first-1.0-SNAPSHOT.jar .
+COPY second/target/second-1.0-SNAPSHOT.jar .
+
+RUN jlink --strip-debug \
+            --compress 2 \
+            --no-header-files \
+            --no-man-pages \
+            --output min-jdk \
+            --add-modules second \ 
+            --launcher run=second/com.rafabene.second.App \ 
+            --module-path . 
+
+FROM panga/alpine:3.7-glibc2.25
+
+WORKDIR /app
+
+COPY --from=builder /app/min-jdk/ /app/min-jdk/
+
+ENTRYPOINT ["/app/min-jdk/bin/run"]
--- second/src/main/java/com/rafabene/second/App.java
+++ second/src/main/java/com/rafabene/second/App.java
@@ -6,16 +6,15 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "I'm second" );
-        System.out.println(new com.rafabene.first.App().first());
-        System.out.print("Using \"first\" internal API: ");
-        System.out.println(new com.rafabene.first.internal.Util().utilMethod());    
-        try (Scanner  s = new Scanner(System.in)){
-            s.nextLine();
-        }
+public class App {
+    public static void main(String[] args) {
+        new App().second();
     }
+
+    public void second() {
+        System.out.println("I'm second");
+        System.out.println(new com.rafabene.first.App().first());
+        System.out.println("Using \"first\" internal API:" +  new com.rafabene.first.internal.Util().utilMethod());
+    }
+
 }
